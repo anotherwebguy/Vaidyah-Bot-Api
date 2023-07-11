5e764be5855a94485d9d008f4d4dccb9e586b3e1
Mohit <mohitrajputms123@gmail.com>
2023-07-11 15:52:59 +0530

    added features info

--- info_api/views.py
+++ info_api/views.py
@@ -1,10 +1,12 @@
 import pymongo
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-import os
+from dotenv import dotenv_values
 
-MONGO_USERNAME = os.environ.get('MONGO_USERNAME')
-MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
+env_vars = dotenv_values(".env")
+
+MONGO_USERNAME = env_vars['MONGO_USERNAME']
+MONGO_PASSWORD = env_vars['MONGO_PASSWORD']
 
 connection_string = "mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}@cluster0.jbnvywb.mongodb.net/?retryWrites=true&w=majority"
 
